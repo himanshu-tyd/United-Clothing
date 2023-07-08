d4b253662bb3c2c1f7eb3b1cf05866e14087be1e
himanshutaviyad0072@gmail.com <himanshutaviyad0072@gmail.com>
2023-07-08 20:36:49 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 import { Switch, Route , } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shop";
+import Header from "./components/header/header";
 
 
 
 const App=()=> {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/shop" component={ShopPage} />
         <Route path="/" component={HomePage} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.styles.scss";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/UNITED.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link className="logo-container">
+        <Logo className="logo"></Logo>
+      </Link>
+
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
